b0b48dce4b776abcf0acc4ba0a8d94dcc3f6019b
vi <yaremenko.v@gmail.com>
2016-01-30 09:28:12 +0200

    new description+style

--- js/index.js
+++ js/index.js
@@ -4,4 +4,8 @@
 
 $(document).ready(function() {
   $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $('.collapsible').collapsible({
+    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
 });
